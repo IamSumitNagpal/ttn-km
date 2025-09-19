b9d0ecdfa1cfb5ef078047e105d074ac66f79636
Sumit Nagpal <sumit.nagpal@frequency.com>
2025-09-19 18:27:12 +0530

    Ack Manual

--- rabbitmq.consumer/src/main/java/com/ttn/rabbitmq/consumer/RabbitMqConsumer.java
+++ rabbitmq.consumer/src/main/java/com/ttn/rabbitmq/consumer/RabbitMqConsumer.java
@@ -1,15 +1,19 @@
 package com.ttn.rabbitmq.consumer;
 
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class RabbitMqConsumer {
-
-
-    @RabbitListener(queues = "first-queue")
-    public void consumeMessage(String message){
+    @RabbitListener(queues = "first-queue", ackMode = "MANUAL")
+    public void consumeMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long tag) throws IOException {
         System.out.println("Received : "+ message);
+        channel.basicAck(tag, false);
     }
 }
